35af4108cd7f90a474a6f4e584ffad115302cb02
YonathanZetune <yonathan@zetune.com>
2019-09-06 16:45:10 -0500

    made basic page objects

--- access.py
+++ access.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 # Sending GET request to retrieve list of posts
 res = requests.get('https://jsonplaceholder.typicode.com/posts')
 
@@ -11,3 +11,28 @@ res = requests.get('https://jsonplaceholder.typicode.com/posts')
 
 # Access the title of the 2nd post returned
 print(res.json()[1]["title"])
+
+# Response JSON string object
+json_string = json.loads(res.text)[1]
+
+
+class PageObject(object):
+    userID = 0
+    objID = 0
+    title = ""
+    body = ""
+    # def __init__(self, userID,objID,title,body):
+    #     return pageObject
+    # userID, objID, title, body
+
+def make_page():
+    page = PageObject()
+    page.userID = json_string['userId']
+    page.objID = json_string['id']
+    page.title = json_string['title']
+    page.body = json_string['body']
+    return page
+
+myPage = make_page()
+
+print('My object properties: \nUser ID: ', myPage.userID, '\nID: ', myPage.objID, '\nTitle: ', myPage.title, '\nBody: ', myPage.body)
