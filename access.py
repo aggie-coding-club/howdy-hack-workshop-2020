df26ecd1448fd0e0196746d645967631492a9ce4
Thomas M <thomas.carnegie.1234@gmail.com>
2019-09-06 16:15:18 -0500

    Added comments

--- access.py
+++ access.py
@@ -1,7 +1,13 @@
 import requests
 
-req = requests.get('https://jsonplaceholder.typicode.com/posts')
+# Sending GET request to retrieve list of posts
+res = requests.get('https://jsonplaceholder.typicode.com/posts')
 
-#print(req.content)
-# print(req.json())
-print(req.json()[1]["title"])
+# Display the response as a string
+# print(res.text)
+
+# Print the dictionary form of the JSON returned from the API
+# print(res.json())
+
+# Access the title of the 2nd post returned
+print(res.json()[1]["title"])
